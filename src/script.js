6ffdfa1d182a736dff6081ad6ebeeccf6eea41cc
Malin Winum <mawi@a2g.no>
2023-01-02 08:33:36 +0100

    Updated js.

--- src/script.js
+++ src/script.js
@@ -41,33 +41,30 @@ let showHoursandMinutes = document.querySelector("#currentTime");
 showHoursandMinutes.innerHTML = `${days[today]} ${time}:${minutes}`;
 
 //Gets temperature and City name
+let apiKey = "8e2e3939a15348b12d5c315bd6ba15f3";
 
 function input(event) {
   event.preventDefault();
-  let city = document.querySelector("#searchBar").value;
+  let city = document.getElementById("search-input").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}`).then(showTemperatureAndCity);
 }
 
-let showInput = document.querySelector("#search-form");
+let showInput = document.getElementById("search");
 showInput.addEventListener("submit", input);
 
 function showTemperatureAndCity(response) {
-  let city = response.data.name;
-  let searchedCity = document.querySelector("#searchedCity");
-  searchedCity.innerHTML = `${city}`;
-  let tempCity = Math.round(response.data.main.temp);
-  let currentTemperature = document.querySelector("#currentTemperature");
-  currentTemperature.innerHTML = `${tempCity}°C `;
+  let cityElement = document.querySelector("#searchedCity");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let weatherCondition = document.querySelector("#discription");
+  weatherCondition.innerHTML = response.data.weather[0].description;
+  let windElement = document.querySelector("#windSpeed");
+  windElement.innerHTML = response.data.wind.speed;
+  console.log(response.data.wind.speed);
 }
 
-let city = document.querySelector("#searchedCity").textContent;
-let apiKey = "8e2e3939a15348b12d5c315bd6ba15f3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(`${apiUrl}`).then(showTemperatureAndCity);
-
-/*
 function whereAmI(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -75,15 +72,3 @@ function whereAmI(position) {
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 }
 navigator.geolocation.getCurrentPosition(whereAmI);
-*/
-
-/* 
-OPPGAVE
-
-👨‍🏫 Your task
-In your project, when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.
-Please note: there's no need to include a temperature conversion at the moment. This will be taught later on in the course.
-
-🙀 Bonus point:
-Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.
- */
